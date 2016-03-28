521d10d45079ec08d9084a6f0ec11f901649f14e
zhuangqh <zhuangqhc@163.com>
2016-03-28 16:20:25 +0800

    example for programming with curses

---
+++ chapter7/hello4.c
@@ -0,0 +1,31 @@
+/* hello2.c
+ * purpose   show how to use curses functions with a loop
+ * outline   initialize, draw stuff, wrap up
+ */
+
+#include <stdio.h>
+#include <curses.h>
+#include <unistd.h>
+
+int main() {
+  int i;
+  initscr();
+  clear();
+
+  for (i = 0; i < LINES; ++i) {
+    move(i, i+1);
+    if (i % 2 == 1)
+      standout();
+    addstr("Hello world");
+    if (i % 2 == 1)
+      standend();
+    refresh();
+    sleep(1);
+    move(i, i+1);
+    addstr("              ");    // erase line
+  }
+
+  endwin();
+
+  return 0;
+}
