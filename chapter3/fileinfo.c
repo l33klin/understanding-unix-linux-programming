7102321511117bc183cf102b0ed768cd1134b251
l33klin <l33klin@gmail.com>
2019-09-06 02:01:27 +0800

    fix type warning

--- chapter3/fileinfo.c
+++ chapter3/fileinfo.c
@@ -27,10 +27,10 @@ int main(int argc, char *argv[]) {
  */
 void show_stat_info(char *fname, struct stat *buf) {
   printf("    mode: %o\n", buf->st_mode);
-  printf("   links: %lu\n", buf->st_nlink);
+  printf("   links: %u\n", buf->st_nlink);
   printf("    user: %d\n", buf->st_uid);
   printf("   group: %d\n", buf->st_gid);
-  printf("    size: %ld\n", buf->st_size);
+  printf("    size: %lld\n", buf->st_size);
   printf(" modtime: %ld\n", buf->st_mtime);
   printf("    name: %s\n", fname);
 }
