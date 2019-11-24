2ebc11475e89e82365d8a4553b4da41268a301cc
l33klin <l33klin@gmail.com>
2019-11-24 20:01:22 +0800

    add play_again2.c

--- chapter6/play_again2.c
+++ chapter6/play_again2.c
@@ -1,21 +1,22 @@
-/* play_again1.c
+/* play_again2.c
  * purpose: ask if user wants another transaction
  * method: set tty into char-by-char mode, read char, return result
  * returns: 0=>yes, 1=>no
- * better: do no echo inappropriate input
+ * better: timeout if user walks away
  * authors: klin
- * create at: 2019-11-23
+ * create at: 2019-11-24
  */
+
 
 #include <stdio.h>
 #include <termios.h>
 
 #define QUESTION "Do you want another transaction"
 
-main() {
+int main() {
     int response;
     tty_mode(0);                        /* save tty mode */
-    set_crmode();                       /* set chr-by-chr mode */
+    set_cr_noecho_mode();                       /* set chr-by-chr mode */
     response = get_response(QUESTION);  /* get some answer */
     tty_mode(1);                        /* restore tty mode */
     return response;
@@ -33,27 +34,27 @@ int get_response(char *question) {
             case 'N':
             case EOF:
                 return 1;
-            default:
-                printf("\ncannot understand %c, ", input);
-                printf("Please type y or n \n");
         }
     }
 }
 
-set_crmode()
+set_cr_noecho_mode()
 /*
- * purpose: put file description 0 (i.e. stdin) into chr-by-chr mode
+ * purpose: put file description 0 (i.e. stdin) into chr-by-chr and no echo mode
  * method: use bits in termios
  */
 {
     struct termios ttystate;
     tcgetattr(0, &ttystate);            /* read curr. settings */
     ttystate.c_lflag &= ~ICANON;        /* no buff */
+    ttystate.c_lflag &= ~ECHO;          /* no echo either */
     ttystate.c_cc[VMIN] = 1;            /* get 1 char at a time */
     tcsetattr(0, TCSANOW, &ttystate);   /* install settings*/
 }
 
-tty_mode(int how) {
+int tty_mode(int how)
+/* how==0 => save current mode, how==1 => restore mode*/
+{
     static struct termios original_mode;
     if (how == 0) {
         tcgetattr(0, &original_mode);
@@ -61,3 +62,4 @@ tty_mode(int how) {
         return tcsetattr(0, TCSANOW, &original_mode);
     }
 }
+
